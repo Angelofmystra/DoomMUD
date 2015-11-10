3ef2523def66b1bc7ffae3c88a21d00c20123eb8
Angelofmystra <raakarth@gmail.com>
2015-11-10 17:39:13 +0000

    Directory added properly

---
+++ DoomMUD/Game.cpp
@@ -0,0 +1,42 @@
+#include <boost/lambda/lambda.hpp>
+#include <boost/program_options.hpp>
+
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main(int argc, char *argv[])
+{
+  namespace po = boost::program_options;
+  using namespace std;
+
+  // Declare the supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "produce help message")
+    ("gm", "start the game with gm commands enabled")
+    ("cheat", "start the game with cheats enabled")
+    ("fullscreen", "forces the game to start in full screen mode")
+    ("worldsizex", po::value<int>(), "sets the x dimension of the world")
+    ("worldsizey", po::value<int>(), "sets the y dimension of the world")
+    ("hijack", "take control of an existing instance of the game, if any, instead of complaining about already running")
+    ("load", po::value<std::string>(),"loads a previous game state")
+    ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    cout << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count("worldsizex")) {
+    cout << "World size level was set to "
+      << vm["worldsizex"].as<int>() << ".\n";
+  }
+  else {
+    cout << "World size level was not set.\n";
+  }
+}
