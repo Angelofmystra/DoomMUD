568ac5ada70ad5b838be0126d8bf19257f3913e0
Angelofmystra <raakarth@gmail.com>
2015-11-10 17:54:37 +0000

    Updated command line options

--- DoomMUD/Game.cpp
+++ DoomMUD/Game.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help", "produce help message")
+    ("config", po::value<std::string>(), "path to the config file")
     ("gm", "start the game with gm commands enabled")
     ("cheat", "start the game with cheats enabled")
     ("fullscreen", "forces the game to start in full screen mode")
@@ -21,7 +22,13 @@ int main(int argc, char *argv[])
     ("worldsizey", po::value<int>(), "sets the y dimension of the world")
     ("hijack", "take control of an existing instance of the game, if any, instead of complaining about already running")
     ("load", po::value<std::string>(),"loads a previous game state")
+    ("verbosity", po::value<std::string>()->implicit_value("0"), "verbosity level")
     ;
+
+  // Declare a group of options that will be 
+  // allowed both on command line and in
+  // config file
+  po::options_description config("Configuration");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
